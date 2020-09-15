f7b73783746503ad723f9acbcef1d0da5e762608
c0braKai <51086253+MK-Kaiser@users.noreply.github.com>
2020-09-15 14:23:27 -0500

    Create binaryAddition.py

---
+++ binaryAddition.py
@@ -0,0 +1,16 @@
+def add_binary(a,b):
+    return bin(a+b)[2:]
+    
+    
+Test.assert_equals(add_binary(1,1),"10")
+Test.assert_equals(add_binary(0,1),"1")
+Test.assert_equals(add_binary(1,0),"1")
+Test.assert_equals(add_binary(2,2),"100")
+Test.assert_equals(add_binary(51,12),"111111")
+
+
+'''
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+'''
