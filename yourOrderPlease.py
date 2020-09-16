de3714eeb0f6e0434f354a8f64c6e5f155fbbed9
c0braKai <51086253+MK-Kaiser@users.noreply.github.com>
2020-09-16 07:15:19 -0500

    Create yourOrderPlease.py

---
+++ yourOrderPlease.py
@@ -0,0 +1,33 @@
+'''
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+Examples
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+'''
+
+
+
+def order(sentence):
+    new_sentence = ""
+    if sentence == "":
+        return ""
+    else:
+        for i in range(1,9):
+            for word in sentence.split():
+                if str(i) in word:
+                    new_sentence += word
+                    new_sentence += " "
+        return new_sentence[:len(new_sentence)-1]
+        
+        
+        
+        
+Test.assert_equals(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+Test.assert_equals(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+Test.assert_equals(order(""), "")        
